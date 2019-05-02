32b9d4a8981860d68be66601c0100cd57d00c22d
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2019-05-02 15:05:36 -0500

    pull in ANIMALS from API and use setState hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Comet" animal="Cat" breed="American Shorthair" />
-      <Pet name="Kirby" animal="Cat" breed="American Shorthair" />
-      <Pet name="Bjorn" animal="Robot" breed="Floor Sweeper" />
+      <SearchParams />
     </div>
   );
 };
