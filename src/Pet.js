13806ab0db69d75f63b2d9e00fc6a6aebaee389f
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2019-05-02 11:56:02 -0500

    use destructuring in Pet component

--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export const Pet = props => {
+const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed)
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
   ]);
 };
 
